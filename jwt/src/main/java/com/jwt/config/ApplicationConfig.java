8c6cd7193f636f4bbb576e65f70f1a8a7775d49c
bsyso <bsyso@softserveinc.com>
2023-01-08 17:44:54 +0100

    added security 6 setting

---
+++ jwt/src/main/java/com/jwt/config/ApplicationConfig.java
@@ -0,0 +1,20 @@
+package com.jwt.config;
+
+import com.jwt.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+    private final UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> userRepository.findUserByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User with username " + username + "was not found"));
+    }
+}
